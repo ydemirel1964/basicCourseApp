cfe4e00ce4f0c0ecf27100bb3556653e55640c00
Yunus Demirel <ydemirel1964@gmail.com>
2023-05-25 00:32:05 +0300

    app completed

--- appES5.js
+++ appES5.js
@@ -24,9 +24,20 @@ UI.prototype.clearControls = function () {
 UI.prototype.deleteCourse = function (element) {
     if (element.classList.contains('delete')) {
         element.parentElement.parentElement.remove();
-        element.childE
     }
 }
+UI.prototype.showAlert = function (message, className) {
+    var alertHtml = `<div class='alert alert-${className}'>
+    ${message}
+    </div>`;
+    const row = document.querySelector('.row');
+    row.insertAdjacentHTML("beforeBegin", alertHtml);
+
+    setTimeout(() => {
+        document.querySelector(".alert").remove();
+    }, 3000);
+
+};
 
 document.getElementById('new-course').addEventListener('submit', function (event) {
     const title = document.getElementById('title').value;
@@ -36,8 +47,14 @@ document.getElementById('new-course').addEventListener('submit', function (event
     const course = new Course(title, instructor, image);
 
     const ui = new UI();
-    ui.addCourseToList(course);
-    ui.clearControls();
+    if (title === '' || instructor === '' || image === '') {
+        ui.showAlert('Please complete the form', 'warning');
+    } else {
+        ui.showAlert('the course has been added', 'success');
+        ui.addCourseToList(course);
+        ui.clearControls();
+    }
+
     event.preventDefault();
 });
 
@@ -45,4 +62,5 @@ document.getElementById('course-list').addEventListener('click', function (event
 
     const ui = new UI();
     ui.deleteCourse(event.target)
+    ui.showAlert("the course has been deleted", "danger");
 });
